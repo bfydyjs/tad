49762a9c5ac580c799bfb15efc9ca57af8455717
bfydyjs <bfydyjs@petalmail.com>
2026-02-06 13:15:21 +0800

    setup paper style

--- tad/visualization/plot/setup_paper_style.py
+++ tad/visualization/plot/setup_paper_style.py
@@ -1,7 +1,37 @@
 import matplotlib.pyplot as plt
 
 
-def setup_paper_style():
+def get_figsize_from_pt(width_pt, ratio=1.618, fraction=1.0):
+    r"""
+    Convert LaTeX width (pt) to Matplotlib figsize (inches).
+
+    Parameters:
+        width_pt (float): LaTeX \columnwidth or \textwidth in points (pt),
+                          use \typeout{COLUMNWIDTH = \the\columnwidth} or \typeout{TEXTWIDTH = \the\textwidth}
+        ratio (float): Aspect ratio (width/height), default golden ratio 1.618
+        fraction (float): Fraction of width to use (e.g., 0.95 for margins)
+
+    Returns:
+        (width_inch, height_inch): Matplotlib figure dimensions in inches
+
+    Note:
+        System          Definition of Point (pt)       Definition of Inch
+        -----------     ---------------------------    ---------------------------
+        LaTeX           1 inch = 72.27 pt              1 inch = 25.4 mm (Standard International Inch)
+        MATLAB          1 inch = 72 pt                 1 inch = 25.4 mm (Standard International Inch)
+        Matplotlib      1 inch = 72 pt                 1 inch = 25.4 mm (Standard International Inch)
+        Physical World  -                              1 inch = 25.4 mm (International Standard)
+    """
+    inches_per_pt = 1 / 72.27  # pt->inch
+    width_inch = width_pt * inches_per_pt * fraction
+    height_inch = width_inch / ratio
+    return (width_inch, height_inch)
+
+
+def setup_paper_style(textwidth):
+
+    # 自动计算适合当前文档的 figsize
+    figsize = get_figsize_from_pt(textwidth, ratio=1.618, fraction=0.98)  # 0.98 \textwidth
     """
     Journal Standard: Arial/Helvetica.
     Note:
@@ -10,7 +40,7 @@ def setup_paper_style():
     """
     plt.rcParams.update(
         {
-            "figure.figsize": [6.8, 4],  # double-column; single-column → [3.5, 2.6]
+            "figure.figsize": figsize,  # double-column; single-column → [3.5, 2.6]
             "font.family": "sans-serif",
             "font.sans-serif": [
                 "Arial",
@@ -38,6 +68,6 @@ def setup_paper_style():
             "legend.edgecolor": "black",
             "savefig.dpi": 600,
             "savefig.bbox": "tight",
-            "savefig.pad_inches": 0.1,
+            "savefig.pad_inches": 0,
         }
     )
