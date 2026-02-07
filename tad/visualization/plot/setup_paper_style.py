bdc5b1e6a81d4feb4b9f73456896404725acec03
bfydyjs <bfydyjs@petalmail.com>
2026-02-07 18:12:56 +0800

    setup_paper_style

--- tad/visualization/plot/setup_paper_style.py
+++ tad/visualization/plot/setup_paper_style.py
@@ -46,7 +46,7 @@ def get_figsize_from_pt(width_pt, ratio=1.618, fraction=1.0):
     return (width_inch, height_inch)
 
 
-def setup_paper_style(textwidth, ratio, fraction,font_size_tex=10,font_size_main=9, line_width_axis=0.5):
+def setup_paper_style(textwidth, ratio, fraction, font_size_tex=10, font_size_main=9, line_width_axis=1):
     figsize = get_figsize_from_pt(textwidth, ratio, fraction)  # double-column; single-column → textwidth / 2
     plt.rcParams.update(
         {
@@ -69,22 +69,25 @@ def setup_paper_style(textwidth, ratio, fraction,font_size_tex=10,font_size_main
             "axes.spines.right": False,
             "axes.grid": True,
             "xtick.labelsize": font_size_main,
-            "xtick.major.size": 3,
-            "xtick.major.width": line_width_axis,
+            "xtick.major.size": 4 * 0.35 * line_width_axis,
+            "xtick.major.width": 0.35 * line_width_axis,
+            "xtick.major.pad": 0.15 * font_size_main,
             "ytick.labelsize": font_size_main,
-            "ytick.major.size": 3,
-            "ytick.major.width": line_width_axis,
+            "ytick.major.size": 4 * 0.35 * line_width_axis,
+            "ytick.major.width": 0.35 * line_width_axis,
+            "ytick.major.pad": 0.15 * font_size_main,
             "grid.linestyle": "--",
             "grid.color": "#bdbdbd",
-            "grid.linewidth": line_width_axis,
+            "grid.linewidth": 0.35 * line_width_axis,
             "grid.alpha": 0.3,  # Transparency
-            "lines.linewidth": 4 * line_width_axis,
-            "lines.markersize": 4 * 2.5 * line_width_axis,
+            "lines.linewidth": 2 * line_width_axis,
+            "lines.markersize": 3 * 2 * line_width_axis,
             "legend.fontsize": font_size_main,
             "legend.frameon": True,
             "legend.fancybox": True,
-            "legend.framealpha": 0.8,
-            "legend.edgecolor": "black",
+            "legend.framealpha": 0.6,
+            "legend.edgecolor": "#bdbdbd",
+            "patch.linewidth": line_width_axis,
             "savefig.dpi": 600,
             "savefig.bbox": "tight",
             "savefig.pad_inches": 0,
