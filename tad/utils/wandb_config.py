86a93192caedef2b903d9c3e89b7604e4829b7ab
bfydyjs <bfydyjs@petalmail.com>
2026-03-02 22:40:00 +0800

    wandb_config

--- tad/utils/wandb_config.py
+++ tad/utils/wandb_config.py
@@ -1,11 +1,12 @@
 def get_custom_config(cfg, params=None, gflops=None):
-    # 1. return full config
+
     parts = str(cfg.work_dir).strip("/").split("/")
     cfg.group = "_".join(parts[-3:-1])
     if params is not None:
         cfg.Params = f"{params / 1e6:.2f} M"
     if gflops is not None:
         cfg.GFLOPs = gflops
+    # 1. return full config
     return cfg
 
     # 2. return optional selected fields
